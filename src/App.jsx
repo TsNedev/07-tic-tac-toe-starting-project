d7bb1301c02576ce2667f97beddf0efd11f0da2b
TsNedev <tsvetannedev360@gmail.com>
2025-02-04 21:18:28 +0200

    playersSetName

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-<Player name="Player1" symbol="X"/>
+<Player initialName="Player1" symbol="X"/>
 
-<Player name="Player2" symbol="O"/>
+<Player initialName="Player2" symbol="O"/>
         </ol>
+        GAME BOARD
       </div>
     </main>
   )
